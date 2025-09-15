bce03d364ab202ae7abf2534c2d72cb66420c32e
AlejandroVeras <verasetanislao21@gmail.com>
2025-09-15 09:31:49 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ export default function HomePage() {
             {/* Logo */}
             <div className="flex items-center gap-3 animate-in fade-in slide-in-from-left-5 duration-700">
               <img 
-                src="/img/formaslogo.svg" 
+                src="/img/formaslogo.png" 
                 alt="Formas Logo" 
                 className="w-12 h-12 transition-transform hover:scale-110"
               />
@@ -388,7 +388,7 @@ export default function HomePage() {
             <div className="animate-in fade-in slide-in-from-left-5 duration-700">
               <div className="flex items-center gap-3 mb-4">
                 <img 
-                  src="/img/formaslogo.svg" 
+                  src="/img/formaslogo.png" 
                   alt="Formas Logo" 
                   className="w-8 h-8"
                 />
